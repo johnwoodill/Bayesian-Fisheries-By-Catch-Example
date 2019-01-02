bfd85548c1eb629031485635e95b69850fe69b0e
johnwoodill <johnwoodill@gmail.com>
2019-01-02 13:18:25 -0700

    commented code

--- Fisheries_example.py
+++ Fisheries_example.py
@@ -6,22 +6,27 @@
 import pystan
 import pandas as pd
 
+# Load data
 dat = pd.read_csv("/Users/john/pCloud Drive/Projects/Bayesian Fisheries By Catch Example/sim_dat.csv")
 dat.head()
 
+# Create dictionary with variables for stan
 ndat = dict(N = len(dat['year']),year = dat['year'], sets = dat['sets'], inter = dat['inter'], mort = dat['mort'])
-ndat
+
 
 # Fit using poisson_model to estimate bycatch interactions
 sm = pystan.StanModel(file='poisson_model.stan')
 fit = sm.sampling(data=ndat, iter=1000, chains=4)
 
+# Print poisson estimates
 print(fit)
 
+# Plot predictions
 fit.plot()
 
 # Fit using Bionomial to estimate mortality
 sm2 = pystan.StanModel(file='binomial_model.stan')
 fit2 = sm2.sampling(data=ndat, iter=1000, chains=4)
 
+# Print binomial estimates
 print(fit2)
